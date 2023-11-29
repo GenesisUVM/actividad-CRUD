ceb09f88641e1c1ca9f993d872584a97edcf7922
GenesisUVM <129811804+GenesisUVM@users.noreply.github.com>
2023-11-28 22:38:16 -0400

    html y css basico

--- plutus-shop/src/App.js
+++ plutus-shop/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <img src="../scr/" alt="logo" />
+        <h2>Plutus Shop</h2>
+        <nav>
+          <a href="#">Compras</a>
+          <a href="" >Ventas</a>
+        </nav>
+      </header>
+      <section>
+        <h3>Compras</h3>
+      </section>
     </div>
   );
 }
